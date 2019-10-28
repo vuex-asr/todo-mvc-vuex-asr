89cdee1d1b9022628b5fe2ed95393699cfe6859e
Joris Wenting <jorislive@hotmail.com>
2019-10-28 21:06:52 +0100

    WIP()

--- src/main.js
+++ src/main.js
@@ -2,12 +2,35 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import VuexASR from "vuex-asr";
 import "./registerServiceWorker";
+import vuetify from "./plugins/vuetify";
 
+Vue.use(VuexASR);
 Vue.config.productionTip = false;
+
+/**
+ * The following block of code may be used to automatically register your
+ * Vue components. It will recursively scan this directory for the Vue
+ * components and automatically register them with their "basename".
+ *
+ * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
+ */
+
+const files = require.context("./components", true, /\.vue$/i);
+files.keys().map(key =>
+  Vue.component(
+    key
+      .split("/")
+      .pop()
+      .split(".")[0],
+    files(key).default
+  )
+);
 
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount("#app");
--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,53 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
+  state: {
+    todos: [
+      { id: 1, title: "do groceries", description: "", done: false },
+      { id: 2, title: "do wash", description: "with care", done: false },
+      {
+        id: 3,
+        title: "go to hairdresser",
+        description: "tip if you are happy",
+        done: true
+      }
+    ]
+  },
+  getters: {
+    todos: state => {
+      return state.todos;
+    },
+    done: state => {
+      return state.todos.filter(item => {
+        return item.done === true;
+      });
+    }
+  },
+  mutations: {
+    addTodo(state, item) {
+      // create id
+      let max = 0;
+      state.todos.forEach(todo => {
+        if (todo.id > max) {
+          max = todo.id;
+        }
+      });
+
+      item.id = max + 1;
+      item.done = false;
+
+      console.log(item);
+
+      state.todos.push(item);
+    },
+    removeTodo(state, item) {
+      console.log('removeTodo was triggered');
+      console.log(state.todos);
+      state.todos = state.todos.filter(todo => {
+        return todo.id !== item.id;
+      });
+    }
+  },
   actions: {},
   modules: {}
 });
