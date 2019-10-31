1121525da82a18ea0430dcf6688b03c33b142a01
Joris Wenting <jorislive@hotmail.com>
2019-10-31 12:30:24 +0100

    chore(): cleanup ListTodos

--- src/components/ListTodos.vue
+++ src/components/ListTodos.vue
@@ -10,28 +10,3 @@
     </ul>
   </div>
 </template>
-
-<script>
-export default {
-  computed: {
-    // todosSelected: function() {
-    //
-    //   let todosSelected = this.todos;
-    //
-    //   switch (this.visibility) {
-    //     case "active":
-    //       todosSelected = this.todosActive;
-    //       break;
-    //     case "completed":
-    //       todosSelected = this.todosDone;
-    //       break;
-    //   }
-    //
-    //   return todosSelected;
-    // }
-  },
-  // filters: {
-  //   capitalize: s => s.charAt(0).toUpperCase() + s.slice(1)
-  // }
-};
-</script>
