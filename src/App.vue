f18f7e35c92cebe71cbdc722c6e7c64cbda31491
Joris Wenting <jorislive@hotmail.com>
2019-11-04 20:41:53 +0100

    chore(): add comments to App file

--- src/App.vue
+++ src/App.vue
@@ -3,25 +3,35 @@
     <header class="header">
       <h1>todos</h1>
     </header>
-    <new-todo
-      asr-bind-state="Todos/newTodo"
-      asr-bind-actions="Todos/addTodo"
-    />
-    <list-todos
-      asr-bind-getters="Todos/todosSelected AS todos"
-      asr-bind-mutations="
+
+      <!-- Input for new todos -->
+
+      <new-todo
+              asr-bind-state="Todos/newTodo"
+              asr-bind-actions="Todos/addTodo"/>
+
+      <!-- List Todos -->
+
+      <list-todos
+              asr-bind-getters="Todos/todosSelected AS todos"
+              asr-bind-mutations="
         Todos/removeTodo,
         Todos/editTodo,
         Todos/toggleTodo,
         Todos/deleteTodo"
-      asr-debug
-    />
-    <todo-footer
-      asr-bind-state="Todos/visibility, Todos/filters"
-      asr-bind-getters="Todos/todosActive"
-      asr-bind-mutations="Todos/changeVisibility"
-      asr-debug
-    />
+              asr-debug
+      />
+
+      <!-- Info and filters -->
+
+      <todo-footer
+              asr-bind-state="
+        Todos/visibility,
+        Todos/filters"
+              asr-bind-getters="Todos/todosActive"
+              asr-bind-mutations="Todos/changeVisibility"
+              asr-debug
+      />
   </div>
 </template>
 
